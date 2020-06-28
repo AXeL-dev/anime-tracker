df9962d1a410ca80cc47105d1c692a4179dc7c74
AXeL-dev <contact.axel.dev@gmail.com>
2020-06-28 15:24:52 +0100

    Update popup main component

--- src/app/modules/popup/main/main.component.ts
+++ src/app/modules/popup/main/main.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  open = false;
+  headerType = 'header';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDrawer() {
+    this.open = !this.open;
   }
 
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@blox/material';
 
 
@@ -8,9 +9,11 @@ import { MaterialModule } from '@blox/material';
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
     MaterialModule
   ],
   exports: [
+    FormsModule,
     MaterialModule
   ]
 })
