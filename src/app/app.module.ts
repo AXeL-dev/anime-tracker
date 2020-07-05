6a24d6366cf94cc9021b4633eb962476ed8b6aef
AXeL-dev <contact.axel.dev@gmail.com>
2020-07-05 23:13:15 +0100

    Fix issues on chrome
    1) width & height issue
    2) settings load issue (was undefined on load)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SettingsService } from './services/settings.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,10 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    // Init settings service on app initialisation
+    { provide: APP_INITIALIZER, useFactory: SettingsService.init, deps: [SettingsService], multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Settings } from '../models/settings';
 import { Proxy } from '../models/proxy';
 import { StorageService } from './storage.service';
+import { debug } from '../helpers/debug.helper';
 
 @Injectable({
   providedIn: 'root',
@@ -34,12 +35,17 @@ export class SettingsService {
     // },
   ];
 
-  constructor(private storage: StorageService) {
-    this.get();
+  constructor(private storage: StorageService) { }
+
+  static init(self: SettingsService) {
+    return () => new Promise(resolve => {
+      self.get().then(() => resolve());
+    });
   }
 
   private async get() {
     const settings = await this.storage.get('settings');
+    debug('Settings:', settings);
     const defaults = this.getDefaults();
     this.set({...defaults, ...settings}); // any existing settings value will override defaults
   }
