630e690a959271a06d09de1e515037d05330363d
AXeL-dev <contact.axel.dev@gmail.com>
2020-10-24 15:52:35 +0100

    Moved icon toggle directive to a sub folder

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@blox/material';
-import { IconToggleDirective } from './directives/icon-toggle.directive';
+import { IconToggleDirective } from './directives/icon-toggle/icon-toggle.directive';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 
 
