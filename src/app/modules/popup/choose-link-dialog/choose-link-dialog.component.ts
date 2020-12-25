c92c937652e2b75593c0cdd33fb516f93d963d8d
AXeL-dev <contact.axel.dev@gmail.com>
2020-12-25 18:32:03 +0100

    Set custom colors for links badge

--- src/app/modules/popup/choose-link-dialog/choose-link-dialog.component.ts
+++ src/app/modules/popup/choose-link-dialog/choose-link-dialog.component.ts
@@ -16,6 +16,12 @@ export class ChooseLinkDialogComponent implements OnInit {
   @Output() linkClick: EventEmitter<void> = new EventEmitter();
   @Output() close: EventEmitter<void> = new EventEmitter();
   @ViewChild('dialog') private dialog: MdcDialogDirective;
+  private badgeColors: {[key: string]: string} = {
+    'vostfr': 'blue',
+    'vostar': 'yellow',
+    'vosten': 'dark',
+    'vf': 'green'
+  };
 
   constructor(private settings: SettingsService, private browser: BrowserService) { }
 
