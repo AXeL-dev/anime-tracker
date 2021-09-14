d23d67eb42de5cfbedbae7dc0f7bd14292e8be98
AXeL-dev <contact.axel.dev@gmail.com>
2021-09-14 13:01:06 +0100

    chore: update MavAnimes crawler

--- src/app/crawlers/vostfr/mavanimes.crawler.ts
+++ src/app/crawlers/vostfr/mavanimes.crawler.ts
@@ -13,14 +13,17 @@ export class MavAnimesCrawler extends LatestEpisodesCrawler {
     this.filters = {
       ...this.filters,
       cover: (text: string) => {
-        return text.replace('-300x169', '');
+        const url = text.indexOf(',') !== -1 ?
+          text.split(',')[0].replace(/ (\d+\w)$/, '') :
+          text.replace('-300x169', '');
+        return url;
       },
       number: (text: string) => {
         const num = text.replace('VOSTFR', '').match(/(.*) (\d+)/);
         return num?.length ? +num[2] : 1;
       },
       title: (text: string) => {
-        return text.replace('VOSTFR', '').replace(/(.*) (\d+)/, '$1');
+        return text.replace('VOSTFR', '').replace(/(.*) (\d+)/, '$1').replace(/ – $/, '');
       },
       subtitles: (text: string) => {
         return 'vostfr';
@@ -35,7 +38,7 @@ export class MavAnimesCrawler extends LatestEpisodesCrawler {
       {
         anime: {
           title: 'p | title',
-          cover: 'img.wp-post-image@src,src-set,data-cfsrc | cover',
+          cover: 'img.wp-post-image@srcset,src,data-cfsrc | cover',
         },
         number: 'p | number',
         streamLinks: [
