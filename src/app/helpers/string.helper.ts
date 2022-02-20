37c74146f586de5d8987de28f7c6bc6dfa63fddf
AXeL-dev <contact.axel.dev@gmail.com>
2022-02-20 23:09:06 +0100

    chore: improved episode titles similarity check

--- src/app/helpers/string.helper.ts
+++ src/app/helpers/string.helper.ts
@@ -1,3 +1,5 @@
+import { compareTwoStrings } from 'string-similarity';
+
 // Stolen from: https://github.com/lodash/lodash/blob/master/isString.js
 export function isString(val: any) {
   return (
@@ -21,49 +23,8 @@ export function isSimilar(
     (!strict &&
       (cleanedS1.indexOf(cleanedS2) !== -1 ||
         cleanedS2.indexOf(cleanedS1) !== -1)) ||
-    similarity(cleanedS1, cleanedS2) >= degree
+    compareTwoStrings(cleanedS1, cleanedS2) >= degree
   );
-}
-
-// Stolen from: https://stackoverflow.com/a/36566052
-function similarity(s1: string, s2: string) {
-  let longer = s1;
-  let shorter = s2;
-  if (s1.length < s2.length) {
-    longer = s2;
-    shorter = s1;
-  }
-  let longerLength = longer.length;
-  if (longerLength == 0) {
-    return 1.0;
-  }
-  return (longerLength - editDistance(longer, shorter)) / longerLength;
-}
-
-function editDistance(s1: string, s2: string) {
-  s1 = s1.toLowerCase();
-  s2 = s2.toLowerCase();
-
-  let costs = new Array();
-  for (let i = 0; i <= s1.length; i++) {
-    let lastValue = i;
-    for (let j = 0; j <= s2.length; j++) {
-      if (i == 0) costs[j] = j;
-      else {
-        if (j > 0) {
-          let newValue = costs[j - 1];
-          if (s1.charAt(i - 1) != s2.charAt(j - 1))
-            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-          costs[j - 1] = lastValue;
-          lastValue = newValue;
-        }
-      }
-    }
-    if (i > 0) {
-      costs[s2.length] = lastValue;
-    }
-  }
-  return costs[s2.length];
 }
 
 export function slugify(text: string, specialCharsReplacement?: string) {
