7e823d7d88a650b8ac4a3293bc153c8e7cea410d
axel-dev <contact.axel.dev@gmail.com>
2022-11-22 17:28:23 +0100

    fix: select component text-indent on Firefox

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -22,8 +22,18 @@ for (let file of files) {
   changeFile(file, searchForString, replaceWithString);
 }
 
+// Fix select component text-indent on Firefox
+let file = './node_modules/@material/select/mdc-select.scss';
+searchForString = `// counteracts the extra text padding that Firefox adds by default
+    @-moz-document url-prefix("") {
+      text-indent: -2px;
+    }`;
+replaceWithString = '';
+
+changeFile(file, searchForString, replaceWithString);
+
 // Remove unused material icons (to reduce bundle size)
-let file = './node_modules/material-icons/iconfont/material-icons.scss';
+file = './node_modules/material-icons/iconfont/material-icons.scss';
 let lines = [
   "@import 'round';",
   "@import 'sharp';",
